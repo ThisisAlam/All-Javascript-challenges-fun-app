c5cb83d4587841dd0c760bec52e7b29f60a9ff56
ThisisAlam <103932559+ThisisAlam@users.noreply.github.com>
2026-05-20 13:38:05 +0500

    update the codebase

--- javescript-challenges/src/components/NotFizzBuzz.jsx
+++ javescript-challenges/src/components/NotFizzBuzz.jsx
@@ -4,16 +4,16 @@ export default function NotFizzBuzz() {
         return <ul>{
             number.map(num=>{
                 if(num % 3 === 0){
-                    return <li>{"Player" + {num}}: "Vacation!"</li>;
+                    return <li>{"Player with id (" + num + ") gets a :"} "Vacation!"</li>;
                 }
                 else if(num % 5 === 0){
-                    return <li>{"Player" + {num}}: "$100,000 bonus!"</li>;
+                    return <li>{"Player with id (" + num + ") gets a :"} "$100,000 bonus!"</li>;
                 }
                 else if(num % 3 === 0 && num % 5 === 0){
-                    return <li>{"Player" + {num}}: "JACKPOT! 1 Million and a Yacht!"</li>;
+                    return <li>{"Player with id (" + num + ") gets a :"} "JACKPOT! 1 Million and a Yacht!"</li>;
                 } 
                 else{
-                    return <li>{"Player" + {num}}: "Better luck next time! 🙁"</li>;
+                    return <li>{"Player with id (" + num + ") gets a :"} "Better luck next time! 🙁"</li>;
                 }
             })
         }</ul>
@@ -26,11 +26,11 @@ export default function NotFizzBuzz() {
             <p>Enter player numbers to see their rewards:</p>
             <input type="text" 
                 placeholder="Enter player numbers separated by commas"
-                value={value} 
+                value={value}   
                 onChange={(e) => setValue(e.target.value)}/>
             <button onClick={() => 
                 setResult(notFizzBuzzFunction(
-                    ...value.split(',').map(num => parseInt(num.trim()))
+                    ...value.split(',').map(num=>parseInt(num.trim()))
                 ))}>Check Rewards</button>
             <div style={{marginTop:"20px"}}>
                 {result}
