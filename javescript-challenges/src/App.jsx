a711ea94e46ad4302ff74bc0b0da0068b5a2e577
ThisisAlam <103932559+ThisisAlam@users.noreply.github.com>
2026-05-20 13:30:02 +0500

    updated the codebase

--- javescript-challenges/src/App.jsx
+++ javescript-challenges/src/App.jsx
@@ -3,6 +3,7 @@ import WishperFunction from "./components/WishperFunction";
 import RandomCapsFunction from "./components/RandomCapsFunction";
 import TitleCaseWord from "./components/TitleCaseWord";
 import TitleCaseSentence from "./components/TitleCaseSentence";
+import NotFizzBuzz from "./components/NotFizzBuzz";
 import "./App.css";
 
 export default function App() {
@@ -37,11 +38,15 @@ export default function App() {
         <TitleCaseWord />
         
         <hr />
+        
         {/* //function to "Title Case Sentence" */}
         <TitleCaseSentence />
         
         <hr />
-      
+        
+        {/* //function to "Not FizzBuzz" */}
+        <NotFizzBuzz />
+
       </main>
       <footer>
         <p>Created by <a href="https://github.com/ThisisAlam">Fakhar Alam</a></p>
---
+++ javescript-challenges/src/components/NotFizzBuzz.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+export default function NotFizzBuzz() {
+    function notFizzBuzzFunction(...number) {
+        return <ul>{
+            number.map(num=>{
+                if(num % 3 === 0){
+                    return <li>{"Player" + {num}}: "Vacation!"</li>;
+                }
+                else if(num % 5 === 0){
+                    return <li>{"Player" + {num}}: "$100,000 bonus!"</li>;
+                }
+                else if(num % 3 === 0 && num % 5 === 0){
+                    return <li>{"Player" + {num}}: "JACKPOT! 1 Million and a Yacht!"</li>;
+                } 
+                else{
+                    return <li>{"Player" + {num}}: "Better luck next time! 🙁"</li>;
+                }
+            })
+        }</ul>
+    }
+    const [value, setValue] = React.useState("");
+    const [result, setResult] = React.useState("");
+    
+    return(
+        <div className="not-fizz-buzz-game">
+            <p>Enter player numbers to see their rewards:</p>
+            <input type="text" 
+                placeholder="Enter player numbers separated by commas"
+                value={value} 
+                onChange={(e) => setValue(e.target.value)}/>
+            <button onClick={() => 
+                setResult(notFizzBuzzFunction(
+                    ...value.split(',').map(num => parseInt(num.trim()))
+                ))}>Check Rewards</button>
+            <div style={{marginTop:"20px"}}>
+                {result}
+            </div>
+        </div>
+    )
+}
